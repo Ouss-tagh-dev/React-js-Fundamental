76eea3bc7b0e2d4ce628ae77f3a21b57911d5e40
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-22 11:52:50 +0100

    State Manipulating

--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -12,14 +12,24 @@ class Learning extends Component {
     };
   }
   clickHandler = () => {
-    console.log("Hello you clicked btn");
-    alert("Hello you clicked btn");
+    // console.log("Hello you clicked btn");
+    // alert("Hello you clicked btn");
+    // this.state.fruits[0] = 'Banana'
+    this.setState({
+      fruits: [
+        { name: "Banana", weight: "120gm" },
+        { name: "grape", weight: "20gm" },
+        { name: "Strawberry", weight: "60gm" },
+      ],
+    });
   };
   render() {
     return (
       <div>
         <button onClick={this.clickHandler}>Click Me</button>
         <h1>Fruite name is {this.state.fruits[0].name}</h1>
+        <h1>Fruite name is {this.state.fruits[1].name}</h1>
+        <h1>Fruite name is {this.state.fruits[2].name}</h1>
       </div>
     );
   }
