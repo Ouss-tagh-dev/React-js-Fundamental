de09fbcee21acd04fc5cab44c2b815aab6ad0fd1
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-23 15:59:07 +0100

    Multiple Input

--- src/App.js
+++ src/App.js
@@ -2,27 +2,46 @@ import { Component } from "react";
 class App extends Component {
   state = {
     username: "",
-  };
-  changeData = (e) => {
-    let val = e.target.name; //Username
-    let myValue = e.target.value; //Getting value
-    this.setState({ [val]: myValue }); //value set on the state
+    phonenumber: "",
+    email: "",
+    city: "",
   };
 
-  formSubmit = (e) => {
-    e.preventDefault();
-    console.log("Hi Oussama");
+  changeData = (e) => {
+    let val = e.target.name; // Username, phonenumber, email, city
+    let myValue = e.target.value; //Getting value
+    this.setState({ [val]: myValue }); // Value set on the state
   };
+
   render() {
     return (
       <div className="App">
-        <h1>Hello {this.state.username}</h1>
+        <h1>{this.state.username}</h1>
+        <h1>{this.state.phonenumber}</h1>
+        <h1>{this.state.email}</h1>
+        <h1>{this.state.city}</h1>
         <div>
-          <form onSubmit={this.formSubmit}>
+          <form>
             <div className="form-group" style={{ marginLeft: 200 }}>
               <label>Username</label>
               <br />
               <input type="text" name="username" onChange={this.changeData} />
+              <br />
+              <label>Phone number</label>
+              <br />
+              <input
+                type="text"
+                name="phonenumber"
+                onChange={this.changeData}
+              />
+              <br />
+              <label>Email</label>
+              <br />
+              <input type="text" name="email" onChange={this.changeData} />
+              <br />
+              <label>City</label>
+              <br />
+              <input type="text" name="city" onChange={this.changeData} />
               <br />
               <button type="submit">Submit</button>
             </div>
