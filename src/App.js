1d78c7294e5928b9d8b555aaad7952ed1068ef4b
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-23 19:34:46 +0100

    React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
 import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Button,
+  Breadcrumb,
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+} from "react-bootstrap";
 class App extends Component {
   render() {
-    const names = [
-      { name: "Oussama", age: 21, grad: 5 },
-      { name: "Yassine", age: 20, grad: 3 },
-      { name: "Med", age: 28, grad: 4 },
-    ];
-
-    const items = names.map((item, idx) => {
-      return (
-        <ul key={idx}>
-          <li>
-            {idx} {"Item"}
-            <ul>
-              <li> Name : {item.name}</li>
-              <li> Name : {item.age}</li>
-              <li> Name : {item.grad}</li>
-            </ul>
-            <button className="btn btn-sm btn-success">Edit</button>| |
-            <button className="btn btn-sm btn-danger">Delete</button>
-          </li>
-        </ul>
-      );
-    });
     return (
       <div className="App">
-        <h1>{"Student List"}</h1>
-        <ul className="list">{items}</ul>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                  Link
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Breadcrumb>
+          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+            Library
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>Data</Breadcrumb.Item>
+        </Breadcrumb>
+        
+        <Button variant="primary">Primary</Button>{" "}
+        <Button variant="secondary">Secondary</Button>{" "}
+        <Button variant="success">Success</Button>{" "}
+        <Button variant="warning">Warning</Button>{" "}
+        <Button variant="danger">Danger</Button>{" "}
+        <Button variant="info">Info</Button>{" "}
+        <Button variant="light">Light</Button>{" "}
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
       </div>
     );
   }
