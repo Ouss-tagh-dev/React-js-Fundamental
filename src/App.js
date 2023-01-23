2dd8afe2fd798f1e4ca4fdcac6ceda55ee8f9ed3
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-23 18:06:33 +0100

    Lists

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import { Component } from "react";
 class App extends Component {
-  state = {
-    age: "",
-  };
-
   render() {
+    const names = ["AA", "BB", "CC", "DD", "EE"];
+    const items = names.map((item, idx) => {
+      return (
+        <li key={idx}>
+          {idx} - {item}
+        </li>
+      );
+    });
     return (
       <div className="App">
-        <h1>{"Select Option"}</h1>
-        <div>
-          <form onSubmit={this.formSubmit}>
-            <div className="form-group" style={{ marginLeft: 200 }}>
-              <label>Your Age: </label>
-              <br />
-              <select onChange={(e) => this.setState({ age: e.target.value })}>
-                <option value="" disabled selected>
-                  {" "}
-                  Select Your Age{" "}
-                </option>
-                <option value="15 - 20"> 15 - 20 </option>
-                <option value="21 - 30"> 21 - 30 </option>
-                <option value="31 - 40"> 31 - 40 </option>
-              </select>
-              <h6>My Age : {this.state.age}</h6>
-
-              <button type="submit">Submit</button>
-            </div>
-          </form>
-        </div>
+        <h1>{"Student List"}</h1>
+        <div>{items}</div>
       </div>
     );
   }
