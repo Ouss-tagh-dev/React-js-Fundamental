d76ec2f83d8c67630720a9faf10b683ff340f46a
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-23 13:25:51 +0100

    Single Input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
-import Learning from "./components/Learning";
-function App() {
-  return (
-    <div className="App">
-      <Learning />
-    </div>
-  );
+import { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <div>
+          <form>
+            <div className="form-group">
+              <label>Username</label>
+              <br />
+              <input type="text" name="username" />
+              <br />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
