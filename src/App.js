ed33f9f58e2667147f063d46c8db33c5ef928162
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-23 18:35:11 +0100

    JSON Array List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { Component } from "react";
 class App extends Component {
   render() {
-    const names = ["AA", "BB", "CC", "DD", "EE"];
+    const names = [
+      { name: "Oussama", age: 21, grad: 5 },
+      { name: "Yassine", age: 20, grad: 3 },
+      { name: "Med", age: 28, grad: 4 },
+    ];
+
     const items = names.map((item, idx) => {
       return (
-        <li key={idx}>
-          {idx} - {item}
-        </li>
+        <ul key={idx}>
+          <li>
+            {idx} {"Item"}
+            <ul>
+              <li> Name : {item.name}</li>
+              <li> Name : {item.age}</li>
+              <li> Name : {item.grad}</li>
+            </ul>
+            <button>Edit</button> | <button>Delete</button>
+          </li>
+        </ul>
       );
     });
     return (
