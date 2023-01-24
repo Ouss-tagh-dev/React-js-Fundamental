b88b384779b90470aa24ced23f5abe3b9a481021
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-24 20:14:20 +0100

    Fetch Data From API

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+// const axios = require("axios");
+import axios from "axios";
 class Blog extends Component {
-    render() {
-        return (
-            <div>
-                <h1>This is Blog component</h1>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.setState({ posts: response.data });
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+  render() {
+    const posts = this.state.posts;
+    const allpost = posts.map((post, idx) => {
+      return (
+        <div>
+          <a href="">{post.title}</a>
+          <br />
+          <br />
+          <p>{post.body}</p>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <h1>This is Blog component</h1>
+        <div className="media">
+          <div className="media-body text-center">
+            {allpost}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Blog;
