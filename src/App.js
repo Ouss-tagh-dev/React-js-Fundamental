e26bd6fa9c05fb3299ea3855a7ac9c38f187444b
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-24 19:34:38 +0100

    Router Link Load Component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,11 @@
 import { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Button,
-  Breadcrumb,
-  Navbar,
-  Container,
-  Nav,
-  Form,
-} from "react-bootstrap";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar bg="light" expand="lg">
-          <Container fluid>
-            <Navbar.Brand href="#">React bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <Nav.Link href="#action2">About Us</Nav.Link>
-                <Nav.Link href="#action1">Contact Us</Nav.Link>
-                <Nav.Link href="#action2">Blog Us</Nav.Link>
-                <Nav.Link href="#action1">Profile</Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-
-        <Breadcrumb>
-          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-            Library
-          </Breadcrumb.Item>
-          <Breadcrumb.Item active>Data</Breadcrumb.Item>
-        </Breadcrumb>
+        <h1>This is Home Component Page</h1>
       </div>
     );
   }
---
+++ src/Header.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import App from "./App";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
+import Profile from "./components/Profile";
+export default class Header extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand>
+              <Link to="/">Oussama</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link>
+                  <Link to="/about">About Us</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/contact">Contact Us</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/blog">Blog</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/profile">Profile</Link>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+        <Switch>
+          {/* <Route path="/">
+            <App />
+          </Route>  */}
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
