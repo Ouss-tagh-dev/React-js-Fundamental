939b1b12b3a4c3b4f63f1a87f1fddff7326d2589
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-25 11:59:29 +0100

    Http Post Data (Idea)

--- src/components/Wrtightpost.jsx
+++ src/components/Wrtightpost.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class Writepost extends Component {
+  state = {
+    title: "",
+    body: "",
+  };
+  formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("/posts", {
+        //Where this url will redirect
+        title: this.state.title, //All value which its will tack
+        body: this.state.body,
+      })
+      .then(function (response) { //use for success
+        console.log(response);
+      })
+      .catch(function (error) { //use for error
+        console.log(error);
+      });
+  };
   render() {
     return (
       <center>
         <div>
           <div className="jumbotron col-lg-4">
-            <form>
+            <form onSubmit={this.formSubmit}>
               <div class="form-group ">
                 <label for="title">Title</label>
                 <input
+                  onChange={(event) => {
+                    this.setState({ title: event.target.value });
+                  }}
                   id="title"
                   type="text"
                   class="form-control"
@@ -20,6 +42,9 @@ class Writepost extends Component {
               <div class="form-group">
                 <label for="body">Details</label>
                 <textarea
+                  onChange={(event) => {
+                    this.setState({ body: event.target.value });
+                  }}
                   id="body"
                   type="text"
                   class="form-control"
