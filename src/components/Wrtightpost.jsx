e2febb3ab9dd10d8064c9d5d9920e53e352e23da
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-25 11:37:52 +0100

    Form Components

--- src/components/Wrtightpost.jsx
+++ src/components/Wrtightpost.jsx
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Writepost extends Component {
-    render() {
-        return (
-            <div>
-                <h1>This is Writepost component </h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <center>
+        <div>
+          <div className="jumbotron col-lg-4">
+            <form>
+              <div class="form-group ">
+                <label for="title">Title</label>
+                <input
+                  id="title"
+                  type="text"
+                  class="form-control"
+                  name="title"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label for="body">Details</label>
+                <textarea
+                  id="body"
+                  type="text"
+                  class="form-control"
+                  name="body"
+                  required
+                ></textarea>
+              </div>
+
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </center>
+    );
+  }
 }
 
 export default Writepost;
